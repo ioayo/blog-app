fc6320774678711203cb623629c1d11544a911e0
Bulat <bulawake@gmail.com>
2018-01-08 22:29:56 +0800

    new post form implemented

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -3,10 +3,40 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label htmlFor="title">{field.label}</label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    name="title" 
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+
     render() {
         return (
             <div className="wrapper">
-                Post New
+                <form>
+                    <Field
+                        label="Title"
+                        name="title"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Tags"
+                        name="tags"
+                        component={this.renderField}
+                    />
+                    <Field 
+                        label="Post Content"
+                        name="content"
+                        component={this.renderField}
+                    />
+                </form>
             </div>
         );
     }
